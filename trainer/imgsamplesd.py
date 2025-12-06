911cd8855dac7a012acc2dd44d17d49ea775d95a
ppbrown <phil@bolthole.com>
2025-12-05 22:17:28 -0800

    Make it pick up "custom" pipeline. Also tweak output a bit.

--- trainer/imgsamplesd.py
+++ trainer/imgsamplesd.py
@@ -7,11 +7,12 @@ def argproc():
     p = argparse.ArgumentParser()
     p.add_argument("--model", type=str, required=True)
     p.add_argument("--seed",  type=int, default=90)
-    p.add_argument("--inc_seed",  action="store_true")
+    p.add_argument("--inc_seed",  action="store_true", 
+                   help="auto-increment the sample seed for multi-prompt")
     p.add_argument("--steps",  type=int, default=30)
     p.add_argument("--vae_scaling",  type=float)
-    p.add_argument("--prompt", nargs="+", type=str,
-                   default="woman", help="one or more prompt strings")
+    p.add_argument("--prompt", nargs="+", 
+                   default=["woman"], help="one or more prompt strings")
     p.add_argument("--output_directory", type=str)
     return p.parse_args()
 
@@ -29,13 +30,26 @@ MODEL = args.model
 print(f"Loading from {MODEL}")
 if MODEL.endswith(".safetensors") or MODEL.endswith(".st"):
     raise ValueError("Cannot acccept single-file models. "
-    "Need diffusers directory tree or hf reference")
+        "Need diffusers directory tree or hf reference")
+if os.path.exists(MODEL+"/pipeline.py"):
+    print("Custom pipeline.py detected")
+    custom=MODEL
 else:
-    pipe = DiffusionPipeline.from_pretrained(
-        MODEL, use_safetensors=True,
-        safety_checker=None, requires_safety_checker=False,
-        #torch_dtype=torch.bfloat16,
-    )
+    custom=None
+
+if args.output_directory:
+    OUTDIR=args.output_directory
+else:
+    OUTDIR=MODEL if os.path.isdir(MODEL) else "./"
+print("Will output results to directory", OUTDIR)
+
+
+pipe = DiffusionPipeline.from_pretrained(
+    MODEL, use_safetensors=True,
+    safety_checker=None, requires_safety_checker=False,
+    custom_pipeline=custom,
+    #torch_dtype=torch.bfloat16,
+)
 
 if args.vae_scaling:
     pipe.vae.config.scaling_factor = args.vae_scaling
@@ -58,14 +72,9 @@ if args.inc_seed:
 else:
     generator = [torch.Generator(device="cuda").manual_seed(seed) for _ in range(len(prompt))]
 
-print(f"Trying render of '{prompt}' using seed {seed}...")
+print(f"Trying render of '{prompt}' using seed {seed}")
+
 images = pipe(prompt, num_inference_steps=args.steps, generator=generator).images
-
-if args.output_directory:
-    OUTDIR=args.output_directory
-else:
-    OUTDIR=MODEL if os.path.isdir(MODEL) else "./"
-
 for i,image in enumerate(images):
     meta = PngImagePlugin.PngInfo()
     meta.add_text("Comment", f"prompt={prompt}")
